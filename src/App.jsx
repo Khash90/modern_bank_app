9eb1c25a3c390769ffeb45a004028be454aabf9e
Khash <khashayar.m.003@gmail.com>
2023-07-30 04:45:08 +0430

    planning the road

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import styles from './styles'
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
-      <h1>Hello World</h1>
+    {/* wrapping navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`} >
+          <div className={`${styles.boxWidth}`}>
+              Navbar
+          </div>
+      </div>
+
+    {/* Main part of app */}
+    <div className={`bg-primary ${styles.flexStart}`} >
+          <div className={`${styles.boxWidth}`}>
+            Hero
+          </div>
+    </div>
+
+    {/* Rest of the app */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`} >
+          <div className={`${styles.boxWidth}`}>
+            Stats
+            Business
+            Billing
+            CardDeal
+            Testimonials
+            Clients
+            CTA
+            Footer
+          </div>
+    </div>
+
     </div>
 
   );
